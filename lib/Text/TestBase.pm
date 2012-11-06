4e2f9cc5de8af0b2d8409c1cde1cee5d302f4593
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2012-11-06 08:48:10 +0000

    Import of TOKUHIROM/Text-TestBase-0.05 from CPAN.
    gitpan-cpan-distribution: Text-TestBase
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         TOKUHIROM/Text-TestBase-0.05.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Text/TestBase.pm
+++ lib/Text/TestBase.pm
@@ -2,7 +2,7 @@ package Text::TestBase;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use Class::Accessor::Lite (
     rw => [qw/block_delim data_delim block_class/],
@@ -25,13 +25,25 @@ sub new {
 sub parse {
     my ($self, $spec) = @_;
     my $cd = $self->block_delim;
-    my @hunks = ($spec =~ /^(\Q${cd}\E.*?(?=^\Q${cd}\E|\z))/msg);
+    my $lineno = 1;
+    my @hunks;
+
+    $spec =~ s/
+          ^(\Q${cd}\E.*?(?=^\Q${cd}\E|\z))
+        | ^([^\n]*\n)
+    /
+        if ($1) {
+            push @hunks, $1;
+        } elsif ($2) {
+            $lineno++;
+        }
+        '';
+    /msgxe;
 
     my @blocks;
-    my $lineno = 1;
     for my $hunk (@hunks) {
         push @blocks, $self->_make_block($hunk, $lineno);
-        $lineno += scalar(split /\n/, $hunk) + 1;
+        $hunk =~ s/\n/$lineno++/ge;
     }
     return @blocks;
 }
